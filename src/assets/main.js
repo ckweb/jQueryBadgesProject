8d5f56b11421b8bca3c482520b94301f1831ccec
Christopher Keane <chris@ckweb.com>
2016-12-08 17:37:58 -0500

    First commit

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,19 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/ckweb1230.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+      console.log(response);
+      for(course of response.courses.completed) {
+      	var $htmlContent = $("<div>", {"class": "course"});
+      	$htmlContent.prepend("<h3>" + course.title + "</h3>");
+      	$htmlContent.append("<img src='" + course.badge + "'>");
+      	$htmlContent.append("<a href='" + course.url + "' target='_blank' class='btn btn-primary'>See Course</a>");
+      	$("#badges").append($htmlContent);
+      }
+    }
+  });
 
 });
